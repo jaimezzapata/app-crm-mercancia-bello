f44cafa7067157238ab59e39ce1cf78c6ad5fa61
jaimezzapata <zapataval2304@gmail.com>
2025-03-27 10:21:50 -0500

    Implementación básica del useState en componente Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,25 @@
 import './Login.css'
+import { useState } from 'react'
+import { usuarios } from '../services/database'
+
 function Login() {
+  const [getName, setName] = useState("")
+  const [getPassword, setPassword] = useState("")
+
+  function iniciarSesion() {
+    if (getName === "admin" && getPassword === "admin") {
+      alert("Bienvenido")
+    } else {
+      alert("Usuario o contraseña incorrectos")
+    }
+  }
+
   return (
-    <form className="form">
+    <form /* onSubmit={iniciarSesion} */ className="form">
       Sign Up
-      <input type="text" className="input" placeholder="Name" />
-      <input type="text" className="input" placeholder="Password" />
-      <button>Submit</button>
+      <input onChange={(e) => setName(e.target.value)} type="text" className="input" placeholder="Name" />
+      <input onChange={(e) => setPassword(e.target.value)} type="text" className="input" placeholder="Password" />
+      <button type='button' onClick={iniciarSesion}>Submit</button>
     </form>
   )
 }
---
+++ src/services/database.jsx
@@ -0,0 +1,12 @@
+export let usuarios = [
+  {
+    usuario: "admin",
+    contrasena: "admin123",
+    nombre: "Jaime Zapata"
+  },
+  {
+    usuario: "generico",
+    contrasena: "12345",
+    nombre: "Usuario"
+  },
+]
