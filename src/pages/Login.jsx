d1425455d9050779e208b45a8384b9f7185df89f
jaimezzapata <zapataval2304@gmail.com>
2025-04-24 08:59:01 -0500

    Implementación de peticion get y hook useEffect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,69 @@
-import './Login.css'
-import { useState } from 'react'
-import { alertaError, alertaRedireccion, generarToken } from '../helpers/funciones'
+import "./Login.css";
+import { useEffect, useState } from "react";
+import {
+  alertaError,
+  alertaRedireccion,
+  generarToken,
+} from "../helpers/funciones";
 import { useNavigate } from "react-router-dom";
-let urlUsuarios = "https://back-json-server-jueves.onrender.com/usuarios"
+let urlUsuarios = "https://back-json-server-jueves.onrender.com/usuarios";
 
 function Login() {
-  const [getName, setName] = useState("")
-  const [getPassword, setPassword] = useState("")
-  const [usuarios, setUsuarios] = useState([])
-  let redireccion = useNavigate()
+  const [getName, setName] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
+  let redireccion = useNavigate();
 
-  function getUsuarios(){
+  function getUsuarios() {
     fetch(urlUsuarios)
+      .then((response) => response.json())
+      .then((data) => setUsuarios(data))
+      .catch((error) => console.log(error));
   }
 
+  useEffect(() => {
+    getUsuarios();
+  }, []);
+  
+  console.log(usuarios);    
+
   function buscarUsuario() {
-    let auth = usuarios.find((item) => item.usuario === getName && item.contrasena == getPassword)
-    localStorage.setItem("usuario", auth.nombre)
+    let auth = usuarios.find(
+      (item) => item.usuario === getName && item.contrasena == getPassword
+    );
+    localStorage.setItem("usuario", auth.nombre);
     console.log(auth);
-    return auth
+    return auth;
   }
 
   function iniciarSesion() {
     if (buscarUsuario()) {
-      localStorage.setItem("token", generarToken())
-      alertaRedireccion(redireccion, "/home", "Bienvenido a la aplicacion")
+      localStorage.setItem("token", generarToken());
+      alertaRedireccion(redireccion, "/home", "Bienvenido a la aplicacion");
     } else {
-      alertaError("Error de credenciales")
+      alertaError("Error de credenciales");
     }
   }
-
 
   return (
     <form /* onSubmit={iniciarSesion} */ className="form">
       Sign Up
-      <input onChange={(e) => setName(e.target.value)} type="text" className="input" placeholder="Name" />
-      <input onChange={(e) => setPassword(e.target.value)} type="text" className="input" placeholder="Password" />
-      <button type='button' onClick={iniciarSesion}>Submit</button>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Name"
+      />
+      <input
+        onChange={(e) => setPassword(e.target.value)}
+        type="text"
+        className="input"
+        placeholder="Password"
+      />
+      <button type="button" onClick={iniciarSesion}>
+        Submit
+      </button>
     </form>
-  )
+  );
 }
-export default Login
+export default Login;
